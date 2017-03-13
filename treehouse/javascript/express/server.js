0dc318e145289beeb77f2d454a1961eb2f1b16df
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2017-03-13 12:34:21 +0300

    json parser - reading POST with json parameters

--- treehouse/javascript/express/server.js
+++ treehouse/javascript/express/server.js
@@ -1,13 +1,24 @@
 const express = require('express'),
-      posts = require('./mock/posts.json');
+      posts = require('./mock/posts.json'),
+      jsonParser = require('body-parser').json;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req,res, next) => {
-    console.log(`${req.query.color}`);
-    next();
-})
+const jsonCheck = (req, res, next) => {
+    if(req.body) console.log(`the sky is ${req.body.color}`);
+    else console.log(`there is no body property`);
+    // console.log(`${req.query.color}`);
+    // req.query - passed via ?getparams
+    // we can assign property to req object and use it later
+    next(); // continue executing the script
+};
+
+app.use(jsonCheck);
+app.use(jsonParser()); // enables post requests and parsing it later
+app.use(jsonCheck);
+
+
 app.get('/', (req, res) => {
     res.send('Yo!');
 });
