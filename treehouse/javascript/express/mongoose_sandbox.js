c8462dab2826dd5139b5b5924ca46fc7d930e231
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2017-03-13 18:48:46 +0300

    Mongoose 💩

--- treehouse/javascript/express/mongoose_sandbox.js
+++ treehouse/javascript/express/mongoose_sandbox.js
@@ -15,16 +15,37 @@ db.once('open', () => {
     const AnimalSchema = new Schema({
         type:  {type: String, default: 'goldfish'},
         color:  {type: String, default: 'golden'},
-        size:  {type: String, default: 'small'},
-        mass:  {type: Number, default: '0.007'},
+        size: String,
+        mass:  {type: Number, default: 0.007},
         name:  {type: String, default: 'Angela'}
     });
+
+    AnimalSchema.pre('save', function(next) {
+        if (this.mass >= 100) {
+            this.size = 'big';
+        } else if (this.mass >=5 && this.mass < 100) {
+            this.size = 'medium'
+        } else {
+            this.size = 'small'
+        }
+        next();
+    });
+    
+    AnimalSchema.statics.findSize = function (size, callback) {
+        //this = Animal
+        return this.find({size: size}, callback);
+    }
+
+    AnimalSchema.methods.findSameColor = function(callback) {
+        //this = document
+        return this.model('Animal').find({color: this.color}, callback);
+    }
 
     const Animal = mongoose.model('Animal', AnimalSchema);
 
     const elephant = new Animal({
         type: 'elephant',
-        size: 'big',
+
         color: 'gray',
         mass: 600,
         name: 'Lawrence'
@@ -33,28 +54,47 @@ db.once('open', () => {
 
     const whale = new Animal({
         type: 'whale',
-        size: 'big',
+
         mass: 190500,
         name: "Fig"
     })
 
+    const animalData = [
+        {
+            type: 'mouse',
+            color: 'gray',
+            mass: 0.035,
+            name: 'Marvin'
+        },
+        {
+            type: 'nutria',
+            color: 'brown',
+            mass: 6.35,
+            name: 'Gretchen'
+        },
+        {
+            type: 'wolf',
+            color: 'gray',
+            mass: 45,
+            name: 'iris'
+        }, 
+        elephant,
+        animal,
+        whale
+    ]
+
     Animal.remove({}, () => {
-        elephant.save((err) => {
-            if (err) console.error(`save failed ${err}`);
-            animal.save((err) => {
-                if (err) console.error(`save failed ${err}`);
-                whale.save((err) => {
-                    Animal.find({size: "big"}, (err, animals)=>{
-                        animals.forEach((animal)=>{
-                            console.log(`${animal.name} the ${animal.color} ${animal.type}`);
-                        })
-                        db.close(() => {
-                            console.log('db connection closed!');
-                        });
+        Animal.create(animalData,( (err) => {
+            Animal.findOne({type: 'elephant'}, (err, elephant)=>{
+                elephant.findSameColor((err, animals) => {
+                    animals.forEach((animal)=>{
+                        console.log(`${animal.name} the ${animal.color} ${animal.type} is a ${animal.size}-sized animal`);
                     })
+                    db.close(() => {
+                        console.log('db connection closed!');
+                    });
                 })
             })
-        });
+        }))
     });
 });
-
