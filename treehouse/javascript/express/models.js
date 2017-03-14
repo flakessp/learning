d13b6f9812dafbcd92890208e16bf81cbccafae6
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2017-03-14 12:10:16 +0300

    static/ instance methods

--- treehouse/javascript/express/models.js
+++ treehouse/javascript/express/models.js
@@ -11,6 +11,34 @@ const mongoose = require('mongoose'),
           createdAt: {type: Date, default: Date.now}, // auto Date creation
           answers: [AnswerSchema]
       }),
-      Question = mongoose.Model("Question", QuestionSchema);
+      Question = mongoose.Model("Question", QuestionSchema),
+      sortAnswers = (a,b) => {
+          //- negative a before b
+          //0 no change
+          //+ positive a after b
+          if (a.votes === b.votes) {
+              return b.updateddAt - a.updateddAt;
+            }
+            return b.votes - a.votes;
+          }
+
+QuestionSchema.pre('save', function(next) {
+    this.answers.sort(sortAnswers);
+    next();
+})
+
+AnswerSchema.method('update', function(updates, callback) {
+    Object.assign(this, updates, {updateddAt: new Date()});
+    this.parent().save(callback);
+});
+
+AnswerSchema.method('vote', function(vote, callback) {
+    if(vote === 'up') {
+        this.votes += 1;
+    } else {
+        this.votes -= 1;
+    }
+    this.parent().save(callback);
+})
 
 module.exports.Question = Question;
